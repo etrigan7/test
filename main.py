dbb39e63714d7dd7e6e9c28df7005f3ee1f1fca2
Sasha Niiakyi <test@gmail.com>
2023-05-30 08:49:40 -0400

    Add sub function

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def mult(a: int, b: int) -> int:
 def minus(a: int, b: int) -> int:
 	return a - b
 
+def sub(a: int, b: int) -> float:
+	return a / b
+
 if __name__ == "__main__":
 	print(sum(2, 6))
 	print(mult(2, 6))
 	print(minus(2, 6))
+	print(sub(2, 6))
